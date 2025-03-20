29550ed81e1253422db2933552c6e49466acb192
Ricky Chen <--global>
2025-03-20 16:43:53 -0400

    working llm custom test

--- test-files/custom_tester_llm.sh
+++ test-files/custom_tester_llm.sh
@@ -3,5 +3,6 @@
 source ../../scripts/defaultvenv/bin/activate
 cd ai-autograding-feedback
 llm_output=$(../../../scripts/defaultvenv/bin/python3 main.py --submission_type jupyter --prompt image_analyze --scope image --assignment ../ --question "Question 5b" --model openai --output stdout)
-escaped_llm_output=$(echo "$llm_output" | sed 's/"/\\"/g')
-echo {\"overall_comment\": \"${escaped_llm_output}\"}
+escaped_llm_output=$(printf "$llm_output" | sed 's/"/\\"/g' | awk '{printf "%s\\n", $0}')
+escaped_llm_output_truncated=${escaped_llm_output%\\n*}
+echo "{\"overall_comment\": \"$(printf "%s" "$escaped_llm_output_truncated")\"}"
